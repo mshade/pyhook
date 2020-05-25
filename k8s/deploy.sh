558ff263156bb4ab479cae031960b7906322a43e
Mike Shade <mshade@mshade.org>
2020-05-25 02:37:06 -0400

    :art: rename deploy yaml, fix paths

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,9 +1,9 @@
 NS=pyhook
 SERVICES="pyhook echo"
 
-kubectl -n ${NS} apply -f deploy.yaml
+kubectl -n ${NS} apply -f pyhook-deploy.yaml
 kubectl -n ${NS} apply -f ingress.yaml
-kubectl -n ${NS} create configmap pyhook --from-file=../src/conf/config.yaml --dry-run=client -o yaml | kubectl -n ${NS} apply -f -
+kubectl -n ${NS} create configmap pyhook --from-file=../app/conf/config.yaml --dry-run=client -o yaml | kubectl -n ${NS} apply -f -
 
 for SERVICE in $SERVICES
 do
