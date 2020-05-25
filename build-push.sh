4de58bd67848e69d7b762407aff5e6c48f7f7083
Mike Shade <mshade@mshade.org>
2020-05-25 01:47:16 -0400

    :rocket: don't build twice

--- build-push.sh
+++ build-push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 IMAGE=mshade/pyhook
-SHA=sha-$(git show --oneline -s | awk '{print $1}' )
+SHA=sha-$(git show --oneline -s | awk '{print $1}')
+
+# Build and test
 docker build -t ${IMAGE}:${SHA} .
 docker tag ${IMAGE}:${SHA} ${IMAGE}:latest
 
 if [ ! -z "$1" ]; then
-  docker build -t ${IMAGE}:${1} .
+  docker tag ${IMAGE}:${SHA} ${IMAGE}:${1} .
   docker push ${IMAGE}:${1}
 fi
 
