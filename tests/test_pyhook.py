101aea874a85b22877a23efe28191b83cd5cfc23
Mike Shade <mshade@mshade.org>
2020-05-25 02:45:36 -0400

    :art: formatting

--- tests/test_pyhook.py
+++ tests/test_pyhook.py
@@ -10,78 +10,97 @@ from app import app
 
 @pytest.fixture
 def client():
-  app.app.config["TESTING"] = True
+    app.app.config["TESTING"] = True
 
-  # an example deployment config
-  deployment = {
-    "deploy": "service-deploy",
-    "image": "mshade/pyhook",
-    "key": "xxx",
-    "ns": "service-test",
-    "valid_tag": "^(v[0-9.]+|sha-[a-z0-9]+)",
-  }
+    # an example deployment config
+    deployment = {
+        "deploy": "service-deploy",
+        "image": "mshade/pyhook",
+        "key": "xxx",
+        "ns": "service-test",
+        "valid_tag": "^(v[0-9.]+|sha-[a-z0-9]+)",
+    }
 
-  app.app.config["deployments"] = {"deployment": deployment}
+    app.app.config["deployments"] = {"deployment": deployment}
 
-  with app.app.test_client() as client:
-    yield client
+    with app.app.test_client() as client:
+        yield client
+
 
 # Import deploy object
-with open('./tests/get_deploy.json', mode='r') as f:
-  from types import SimpleNamespace as Namespace
-  deploy_obj = json.load(f, object_hook=lambda d: Namespace(**d))
+with open("./tests/get_deploy.json", mode="r") as f:
+    from types import SimpleNamespace as Namespace
+
+    deploy_obj = json.load(f, object_hook=lambda d: Namespace(**d))
+
 
 def test_index(client):
-  resp = client.get('/')
-  assert '403' in resp.status
+    resp = client.get("/")
+    assert "403" in resp.status
+
 
 def test_unconfigured_deployment(client):
-  resp = client.get('/404me')
-  assert '404' in resp.status
+    resp = client.get("/404me")
+    assert "404" in resp.status
+
 
 def test_deployment_get_without_key(client):
-  resp = client.get('/deployment')
-  assert '403' in resp.status
+    resp = client.get("/deployment")
+    assert "403" in resp.status
+
 
 def test_deployment_get_with_key(client):
-  with patch('kubernetes.client.AppsV1Api.read_namespaced_deployment') as mock_get:
-    mock_get.return_value = deploy_obj
-    resp = client.get('/deployment?key=xxx')
-  assert "200" in resp.status
+    with patch("kubernetes.client.AppsV1Api.read_namespaced_deployment") as mock_get:
+        mock_get.return_value = deploy_obj
+        resp = client.get("/deployment?key=xxx")
+    assert "200" in resp.status
+
 
 def test_deployment_post_with_bad_key(client):
-  post_data = {
-    "push_data": {"tag": "v0.1.1"},
-  }
-  with patch('kubernetes.client.AppsV1Api.read_namespaced_deployment') as mock_get, \
-      patch('kubernetes.client.AppsV1Api.patch_namespaced_deployment') as mock_update:
-    mock_get.return_value = deploy_obj
-    mock_update.return_value = "200 OK"
-    resp = client.post('/deployment?key=aaa', json=post_data)
-  assert "403" in resp.status
+    post_data = {
+        "push_data": {"tag": "v0.1.1"},
+    }
+    with patch(
+        "kubernetes.client.AppsV1Api.read_namespaced_deployment"
+    ) as mock_get, patch(
+        "kubernetes.client.AppsV1Api.patch_namespaced_deployment"
+    ) as mock_update:
+        mock_get.return_value = deploy_obj
+        mock_update.return_value = "200 OK"
+        resp = client.post("/deployment?key=aaa", json=post_data)
+    assert "403" in resp.status
+
 
 def test_deployment_post_with_key(client):
-  post_data = {
-    "push_data": {"tag": "v0.1.1"},
-  }
-  with patch('kubernetes.client.AppsV1Api.read_namespaced_deployment') as mock_get, \
-      patch('kubernetes.client.AppsV1Api.patch_namespaced_deployment') as mock_update:
-    mock_get.return_value = deploy_obj
-    mock_update.return_value = "200 OK"
-    resp = client.post('/deployment?key=xxx', json=post_data)
-  assert "200" in resp.status
+    post_data = {
+        "push_data": {"tag": "v0.1.1"},
+    }
+    with patch(
+        "kubernetes.client.AppsV1Api.read_namespaced_deployment"
+    ) as mock_get, patch(
+        "kubernetes.client.AppsV1Api.patch_namespaced_deployment"
+    ) as mock_update:
+        mock_get.return_value = deploy_obj
+        mock_update.return_value = "200 OK"
+        resp = client.post("/deployment?key=xxx", json=post_data)
+    assert "200" in resp.status
+
 
 def test_deployment_post_with_key_bad_tag(client):
-  post_data = {
-    "push_data": {"tag": "latest"},
-  }
-  with patch('kubernetes.client.AppsV1Api.read_namespaced_deployment') as mock_get, \
-      patch('kubernetes.client.AppsV1Api.patch_namespaced_deployment') as mock_update:
-    mock_get.return_value = deploy_obj
-    mock_update.return_value = "200 OK"
-    resp = client.post('/deployment?key=xxx', json=post_data)
-  assert "400" in resp.status
+    post_data = {
+        "push_data": {"tag": "latest"},
+    }
+    with patch(
+        "kubernetes.client.AppsV1Api.read_namespaced_deployment"
+    ) as mock_get, patch(
+        "kubernetes.client.AppsV1Api.patch_namespaced_deployment"
+    ) as mock_update:
+        mock_get.return_value = deploy_obj
+        mock_update.return_value = "200 OK"
+        resp = client.post("/deployment?key=xxx", json=post_data)
+    assert "400" in resp.status
+
 
 def test_bad_path(client):
-  resp = client.get('/bad/path')
-  assert "40" in resp.status
+    resp = client.get("/bad/path")
+    assert "40" in resp.status
